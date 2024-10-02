24461733eaf672d020e0fbf4803cad633d0b47a5
raviteja011998 <ravitejakandagatla1998@gmail.com>
2024-10-03 00:12:38 +0530

    added two resources aws instance & security group

--- terraform_code/V2-ec2.tf
+++ terraform_code/V2-ec2.tf
@@ -2,35 +2,35 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH Access"
-
-  ingress { 
-    description = "SSH Access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ssh_port"
-  }
-}
-
 resource "aws_instance" "demo-server" {
-  ami = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-  key_name = "terraform_key"
-  security_groups = ["demo-sg"]
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    ami           = "ami-0ebfd941bbafe70c6"
+    instance_type = "t2.micro"
+    key_name      = "terraform_key"
+    
+    # Reference the security group by its ID
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
 }
 
+resource "aws_security_group" "demo-sg" {
+    name        = "demo-sg"
+    description = "SSH Access"
+
+    ingress { 
+        description = "SSH Access"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ssh_port"
+    }
+}
